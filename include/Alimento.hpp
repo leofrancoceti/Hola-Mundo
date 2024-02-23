85547d9ddb682971ef0a44a4c0264334e6f1e335
leofrancoceti <160191116+leofrancoceti@users.noreply.github.com>
2024-02-23 02:09:10 +0000

    version2

---
+++ include/Alimento.hpp
@@ -0,0 +1,15 @@
+#pragma once
+class Alimento
+{
+   private:
+        int energia;
+   public: 
+            Alimento(int energia){
+    this->energia= energia;
+
+   }
+   ~Alimento(){}
+   int ExtraerEnergia(){
+    return energia;
+   }
+};
